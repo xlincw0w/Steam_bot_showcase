a5bc659d53ccae6559628b570e2d94d192cb2d8a
xlincw0w <khaled.khazem@yahoo.com>
2021-07-25 00:29:55 +0100

    fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 /**
  * @author
@@ -6,20 +6,19 @@ import React from "react";
  **/
 
 const Header = (props) => {
-  return (
-    <div className="header">
-      <div className="image"></div>
-      <div className="details">
-        <h1>HachiBot</h1>
-        <p>
-          Buy/sell <b>TF keys</b> with <b>Bitcoin</b>, <b>Ethereum</b>,{" "}
-          <b>Litcoin</b>, <b>Dogecoin</b>, <b>Bitcoin cash</b>, <b>Cardano</b>,{" "}
-          <b>Polkadot</b>, <b>USD</b>, it is easy, safe and reliable !
-        </p>
-        <button>Add friends</button>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className='header'>
+            <div className='image'></div>
+            <div className='details'>
+                <h1>HachiBot</h1>
+                <p>
+                    Buy/sell <b>TF keys</b> with <b>Bitcoin</b>, <b>Ethereum</b>, <b>Litcoin</b>, <b>Dogecoin</b>, <b>Bitcoin cash</b>, <b>Cardano</b>, <b>Polkadot</b>, <b>USD</b>,
+                    it is easy, safe and reliable !
+                </p>
+                <button>Add friends</button>
+            </div>
+        </div>
+    )
+}
 
-export default Header;
+export default Header
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 /**
  * @author
@@ -7,129 +7,120 @@ import axios from "axios";
  **/
 
 const Section = (props) => {
-  const [data, SetData] = useState(null);
-  const [sellBtc, setSellBtc] = useState(null);
-  const [sellEther, setSellEther] = useState(null);
-  const [sellLit, setSellLit] = useState(null);
-  const [sellDoge, setSellDoge] = useState(null);
-  const [sellBtcCash, setSellBtcCash] = useState(null);
+    const [data, SetData] = useState(null)
+    const [sellBtc, setSellBtc] = useState(null)
+    const [sellEther, setSellEther] = useState(null)
+    const [sellLit, setSellLit] = useState(null)
+    const [sellDoge, setSellDoge] = useState(null)
+    const [sellBtcCash, setSellBtcCash] = useState(null)
 
-  useEffect(() => {
-    const fetch = async () => {
-      await axios
-        .get("https://api.alternative.me/v2/ticker/")
-        .then((res) => {
-          let myMap = new Map(Object.entries(res.data.data));
-          console.log(myMap);
-          let data = [];
-          for (const value of myMap.values()) {
-            data.push(value);
-          }
-          SetData(data);
-          console.log("fetching");
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    };
-    fetch();
-  }, []);
-  useEffect(() => {
-    const fetch = async () => {
-      await axios
-        .get("https://api.alternative.me/v2/ticker/")
-        .then((res) => {
-          let myMap = new Map(Object.entries(res.data.data));
-          console.log(myMap);
-          let data = [];
-          for (const value of myMap.values()) {
-            data.push(value);
-          }
-          SetData(data);
-          console.log("fetching");
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    };
+    useEffect(() => {
+        const fetch = async () => {
+            await axios
+                .get('https://api.alternative.me/v2/ticker/')
+                .then((res) => {
+                    let myMap = new Map(Object.entries(res.data.data))
+                    console.log(myMap)
+                    let data = []
+                    for (const value of myMap.values()) {
+                        data.push(value)
+                    }
+                    SetData(data)
+                    console.log('fetching')
+                })
+                .catch((e) => {
+                    console.log(e)
+                })
+        }
+        fetch()
+    }, [])
+    useEffect(() => {
+        const fetch = async () => {
+            await axios
+                .get('https://api.alternative.me/v2/ticker/')
+                .then((res) => {
+                    let myMap = new Map(Object.entries(res.data.data))
+                    console.log(myMap)
+                    let data = []
+                    for (const value of myMap.values()) {
+                        data.push(value)
+                    }
+                    SetData(data)
+                    console.log('fetching')
+                })
+                .catch((e) => {
+                    console.log(e)
+                })
+        }
 
-    const timer = setInterval(() => {
-      fetch();
-    }, 1000000);
-  }, []);
-  const handleChangeSell = (amount) => {
-    setSellBtc(amount / data[0].quotes.USD.price);
-    setSellEther(amount / data[11].quotes.USD.price);
-    setSellDoge(amount / data[3].quotes.USD.price);
-    setSellLit(amount / data[2].quotes.USD.price);
-    setSellBtcCash(amount / data[32].quotes.USD.price);
-  };
-  return (
-    <div className="section">
-      <div className="title">Key Stocks</div>
-      <div className="cards">
-        <div className="card">
-          <div className="head">
-            <h3>
-              If you sell{" "}
-              <input
-                onChange={(e) => handleChangeSell(e.target.value)}
-                type="number"
-              />{" "}
-              {""}$
-            </h3>
-          </div>
+        const timer = setInterval(() => {
+            fetch()
+        }, 1000000)
+    }, [])
+    const handleChangeSell = (amount) => {
+        setSellBtc(amount / data[0].quotes.USD.price)
+        setSellEther(amount / data[11].quotes.USD.price)
+        setSellDoge(amount / data[3].quotes.USD.price)
+        setSellLit(amount / data[2].quotes.USD.price)
+        setSellBtcCash(amount / data[32].quotes.USD.price)
+    }
+    return (
+        <div className='section'>
+            <div className='title'>Key Stocks</div>
+            <div className='cards'>
+                <div className='card'>
+                    <div className='head'>
+                        <h3>
+                            If you sell $ <input onChange={(e) => handleChangeSell(e.target.value)} type='number' />
+                        </h3>
+                    </div>
 
-          <div className="rates">
-            {data ? (
-              <>
-                <h4>BTC : {sellBtc ? sellBtc : data[0].quotes.USD.price}</h4>
-                <h4>
-                  ETH :{sellEther ? sellEther : data[11].quotes.USD.price}
-                </h4>
-                <h4>DOGE : {sellDoge ? sellDoge : data[3].quotes.USD.price}</h4>
-                <h4>LTC : {sellLit ? sellLit : data[2].quotes.USD.price}</h4>
-                <h4>
-                  BCHA:{sellBtcCash ? sellBtcCash : data[32].quotes.USD.price}
-                </h4>
-              </>
-            ) : null}
-          </div>
-          <button>How to sell</button>
+                    <div className='rates'>
+                        {data ? (
+                            <>
+                                <h4>BTC : {sellBtc ? sellBtc : data[0].quotes.USD.price}</h4>
+                                <h4>ETH :{sellEther ? sellEther : data[11].quotes.USD.price}</h4>
+                                <h4>DOGE : {sellDoge ? sellDoge : data[3].quotes.USD.price}</h4>
+                                <h4>LTC : {sellLit ? sellLit : data[2].quotes.USD.price}</h4>
+                                <h4>BCHA:{sellBtcCash ? sellBtcCash : data[32].quotes.USD.price}</h4>
+                            </>
+                        ) : null}
+                    </div>
+                    <button>How to sell</button>
+                </div>
+                <div className='card'>
+                    <div className='head'>
+                        <h3>Crypto withdrawal fees</h3>
+                    </div>
+
+                    <div className='rates'>
+                        <h4>BTC : 0.0551110000000000</h4>
+                        <h4>ETH : 0.0551110000000000</h4>
+                        <h4>DOGE : 0.0551110000000000</h4>
+                        <h4>ADA : 0.0551110000000000</h4>
+                        <h4>DOT : 0.0551110000000000</h4>
+                    </div>
+                    <button>How to withdrawal</button>
+                </div>
+                <div className='card'>
+                    <div className='head'>
+                        <h3>
+                            If you buy 1 $ <input type='number' />
+                        </h3>
+                    </div>
+
+                    <div className='rates'>
+                        <h4>BTC : 0.0551110000000000</h4>
+                        <h4>ETH : 0.0551110000000000</h4>
+                        <h4>DOGE : 0.0551110000000000</h4>
+                        <h4>ADA : 0.0551110000000000</h4>
+                        <h4>DOT : 0.0551110000000000</h4>
+                    </div>
+                    <button>How to buy</button>
+                </div>
+            </div>
         </div>
-        <div className="card">
-          <div className="head">
-            <h3>Crypto withdrawal fees</h3>
-          </div>
+    )
+}
 
-          <div className="rates">
-            <h4>BTC : 0.0551110000000000</h4>
-            <h4>ETH : 0.0551110000000000</h4>
-            <h4>DOGE : 0.0551110000000000</h4>
-            <h4>ADA : 0.0551110000000000</h4>
-            <h4>DOT : 0.0551110000000000</h4>
-          </div>
-          <button>How to withdrawal</button>
-        </div>
-        <div className="card">
-          <div className="head">
-            <h3>
-              If you buy 1 $ <input type="number" />
-            </h3>
-          </div>
-
-          <div className="rates">
-            <h4>BTC : 0.0551110000000000</h4>
-            <h4>ETH : 0.0551110000000000</h4>
-            <h4>DOGE : 0.0551110000000000</h4>
-            <h4>ADA : 0.0551110000000000</h4>
-            <h4>DOT : 0.0551110000000000</h4>
-          </div>
-          <button>How to buy</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Section;
+export default Section
